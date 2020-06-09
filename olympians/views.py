b7a5550d47691c085d9306a9b498e84c2cf12988
Ohikhuemi Ohiozua <believe.ohiozua@gmail.com>
2020-06-09 15:33:52 -0400

    django version

---
+++ olympians/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics, permissions
+from .models import Company
+from .serializers import CompanySerializer
+
+class CompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset            = Company.objects.all()
+    serializer_class    = CompanySerializer
+    lookup_field        = 'pk'
+    permission_classes = (permissions.AllowAny, )
+
+
+class CompanyListView(generics.ListCreateAPIView):
+    queryset            = Company.objects.all()
+    serializer_class    =  CompanySerializer
+    permission_classes =  (permissions.AllowAny, )
+    
+
+
+
+
